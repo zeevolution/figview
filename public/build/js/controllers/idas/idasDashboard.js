6d589a3e5340c9e6d3951bbf86cf24a09aa2213a
Jose Lima <engcompneto11@gmail.com>
2017-03-02 02:45:08 -0300

    Add getIoTDevices for the Idas Dashboard Controller and UI

--- public/build/js/controllers/idas/idasDashboard.js
+++ public/build/js/controllers/idas/idasDashboard.js
@@ -32,6 +32,7 @@ angular.module('app.controllers')
                 getResultsPage(1);
 
                 $scope.showIdas = function (idas) {
+                    console.log(idas);
                     $scope.idas = idas;
                 };
 
@@ -64,6 +65,37 @@ angular.module('app.controllers')
                             ". Idas Server is not responding...";
                     });
 
-                }
+                };
+
+                $scope.getIoTDevices = function (iotenv) {
+                    var settings = {
+                        "async": true,
+                        "crossDomain": true,
+                        "url": $scope.idas.idas_url_adminport + "iot/devices",
+                        "method": "GET",
+                        "headers": {
+                            "fiware-service": iotenv.Fiware_Service,
+                            "content-type": iotenv.content_type,
+                            "fiware-servicepath": iotenv.Fiware_ServicePath,
+                            "x-auth-token": iotenv.X_Auth_Token
+                        }
+                    };
+
+                    $.ajax(settings).then(function (response) {
+                        console.log(response);
+                        $scope.iotDevices = response;
+                        $scope.devicesEnv = $scope.iotDevices.count + " devices found in the " + iotenv.name + " environment. ";
+                    }, function(error, textStatus){
+                        //console.log(error);
+                        $scope.idasStatus = "Error " + error.status + ": " + error.statusText +
+                            ". Idas Server is not responding...";
+                    });
+
+                };
+
+                $scope.resetGetIoTDevices = function () {
+                    $scope.iotDevices = {};
+                    $scope.devicesEnv = "";
+                };
 
             }]);
